5a71f4020ad7111176748c3ffea8bdd50880d28a
Lucca Freixo Bittencourt <113397205+luccabittencourt@users.noreply.github.com>
2023-10-17 10:44:10 -0300

    elemento implementado

--- ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
@@ -78,7 +78,7 @@ void menu()
 void inicializar()
 {
 
-	// provisÛrio porque n„o libera a memoria usada pela arvore
+	// provis√≥rio porque n√£o libera a memoria usada pela arvore
 	NO* raiz = NULL;
 
 	cout << "Arvore inicializada \n";
@@ -179,10 +179,20 @@ void exibirElementosArvore(NO* no)
 
 void buscarElementoArvore(NO* no, int valor)
 {
+	if (no == NULL) {
+		cout << "O valor n√£o foi encontrado. \n";
+		return;
+	}
 
+	else if (valor == no->valor) {
+		cout << "O valor foi encontrado. \n";
+		return;
+	}
 
+	if (valor < raiz->valor) {
+		buscarElementoArvore(no->esq, valor);
+	}
+	else {
+		buscarElementoArvore(no->dir, valor);
+	}
 }
-
-
-
-
